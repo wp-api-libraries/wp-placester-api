679ecf1a9d271650e4c67fac0ed5b024e776ae8a
Brandon Hubbard <bkhubbard@gmail.com>
2017-02-23 15:27:55 -0800

    started

---
+++ wp-placester-api.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * WP Placester API (
+ *
+ * @package WP-Placester-API
+ */
+
+/*
+* Plugin Name: WP Placester API
+* Plugin URI: https://github.com/wp-api-libraries/wp-placester-api
+* Description: Perform API requests to Placester in WordPress.
+* Author: WP API Libraries
+* Version: 1.0.0
+* Author URI: https://wp-api-libraries.com
+* GitHub Plugin URI: https://github.com/wp-api-libraries/wp-placester-api
+* GitHub Branch: master
+*/
+
+/* Exit if accessed directly */
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+
+if ( ! class_exists( 'PlacesterAPI' ) ) {
+
+	/**
+	 * Placester API Class.
+	 */
+	class PlacesterAPI {
+
+
+		/* LISTINGS. */
+
+		/* ORANIZATIONS. */
+
+		/* PEOPLE. */
+
+
+	}
+
+}
